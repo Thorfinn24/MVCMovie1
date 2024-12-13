1a612cd4e479dba86c60c0cce936a610185b3ee0
dhine <dhine@THORFINN>
2024-12-13 10:04:30 +0530

    Add project files.

---
+++ MVCMovie1/Controllers/DefaultController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMovie1.Models;
+
+namespace MVCMovie1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        TasksEntities db = new TasksEntities();
+
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(MovieTicket movieTicket)
+        {
+            db.MovieTickets.Add(movieTicket);
+            db.SaveChanges();
+
+            ViewBag.Message = string.Format("Hello");
+
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVCMovie1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCMovie1.MvcApplication" Language="C#" %>
